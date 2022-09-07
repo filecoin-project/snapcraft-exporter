a2331b90b7258d98e85553618f2a024496574277
Ian Davis <jungziege@gmail.com>
2022-09-07 01:03:28 -0700

    Working first draft of snapcraft-exporter
    Note: this only work on my local machine because I am logged in to
    snapcraft. We will need to set SNAPCRAFT_STORE_CREDENTIALS per
    https://snapcraft.io/docs/snapcraft-authentication

--- main.go
+++ main.go
@@ -1,28 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type SnapcraftMetricsItem struct {
-	Name              string `json:"name"`
-	Values            []int  `json:"values"`
-	CurrentlyReleased string `json:"currently_released,omitempty"`
+	Name     string `json:"name"`
+	Values   []int  `json:"values"`
+	Released bool   `json:"currently_released,omitempty"`
 }
 
 type SnapcraftMetrics struct {
-	Buckets    string `json:"buckets"`
-	MetricName string `json:"metric_name"`
-	Series     []SnapcraftMetricsItem
-	SnapID     string `json:"snap_id"`
-	Status     string `json:"status"`
+	Buckets    []SnapcraftDate        `json:"buckets"`
+	MetricName string                 `json:"metric_name"`
+	Series     []SnapcraftMetricsItem `json:"series"`
+	SnapID     string                 `json:"snap_id"`
+	Status     string                 `json:"status"`
+}
+
+type SnapcraftDate time.Time
+
+func (date *SnapcraftDate) UnmarshalJSON(b []byte) error {
+	s := strings.Trim(string(b), "\"")
+	t, err := time.Parse("2006-01-02", s)
+	if err != nil {
+		return err
+	}
+	*date = SnapcraftDate(t)
+	return nil
+}
+
+func (date SnapcraftDate) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Time(date))
 }
 
 type SnapcraftCollector struct {
@@ -44,11 +63,11 @@ func newSnapcraftCollector(snapNames []string) *SnapcraftCollector {
 		SnapNames: snapNames,
 		deviceChangeDaily: prometheus.NewDesc("snapcraft_device_change_daily",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. daily_device_change: contains the 3 series representing the number of new, continued and lost devices with the given snap installed compared to the previous day.",
-			[]string{""}, nil,
+			[]string{"change"}, nil,
 		),
 		deviceChangeWeekly: prometheus.NewDesc("snapcraft_device_change_weekly",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. weekly_device_change: similar to the ‘daily_device_change’ metric but operates on a 7 day window. i.e. new contains the number of devices that were seen during the last 7 days but not in the previous 7 day and so on for continued and lost.",
-			[]string{""}, nil,
+			[]string{"change"}, nil,
 		),
 		installBaseByChannelDaily: prometheus.NewDesc("snapcraft_install_base_by_channel_daily",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. installed_base_by_channel: contains one series per channel representing the number of devices with the given snap installed, channels with no data across the entire interval are omitted.",
@@ -99,8 +118,43 @@ func (collector *SnapcraftCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.installBaseByVersionWeekly
 }
 
-func (collector *SnapcraftCollector) Collect(ch chan<- prometheus.Metric) {
+func getSnapcraftMetrics(snapName string, snapMetricName string) SnapcraftMetrics {
+	out, err := exec.Command("snapcraft", "metrics", snapName, "--name", snapMetricName, "--format", "json").Output()
 
+	if err != nil {
+		panic(err)
+	}
+
+	snapcraft := SnapcraftMetrics{}
+	json.Unmarshal([]byte(out), &snapcraft)
+
+	return snapcraft
+}
+
+func collectMetric(snapName string, snapMetricName string, metric *prometheus.Desc, ch chan<- prometheus.Metric) {
+	snapcraftMetrics := getSnapcraftMetrics(snapName, snapMetricName)
+	for _, item := range snapcraftMetrics.Series {
+		for i, date := range snapcraftMetrics.Buckets {
+			m := prometheus.MustNewConstMetric(metric, prometheus.GaugeValue, float64(item.Values[i]), item.Name)
+			m = prometheus.NewMetricWithTimestamp(time.Time(date), m)
+			ch <- m
+		}
+	}
+}
+
+func (collector *SnapcraftCollector) Collect(ch chan<- prometheus.Metric) {
+	for _, snapName := range collector.SnapNames {
+		collectMetric(snapName, "daily_device_change", collector.deviceChangeDaily, ch)
+		collectMetric(snapName, "weekly_device_change", collector.deviceChangeWeekly, ch)
+		collectMetric(snapName, "installed_base_by_channel", collector.installBaseByChannelDaily, ch)
+		collectMetric(snapName, "installed_base_by_country", collector.installBaseByCountryDaily, ch)
+		collectMetric(snapName, "installed_base_by_operating_system", collector.installBaseBySystemDaily, ch)
+		collectMetric(snapName, "installed_base_by_version", collector.installBaseByVersionDaily, ch)
+		collectMetric(snapName, "weekly_installed_base_by_channel", collector.installBaseByChannelWeekly, ch)
+		collectMetric(snapName, "weekly_installed_base_by_country", collector.installBaseByCountryWeekly, ch)
+		collectMetric(snapName, "weekly_installed_base_by_operating_system", collector.installBaseBySystemWeekly, ch)
+		collectMetric(snapName, "weekly_installed_base_by_version", collector.installBaseByVersionWeekly, ch)
+	}
 }
 
 func main() {
@@ -112,14 +166,14 @@ func main() {
 	if metricsPath == "" {
 		metricsPath = "/metrics"
 	}
-	formulaeString := os.Getenv("HOMEBREW_FORMULAE")
-	if formulaeString != "" {
-		formulae := strings.Split(formulaeString, ", ")
-		log.Fatal(homebrewExporter(listenPort, metricsPath, formulae))
+	snapNamesString := os.Getenv("SNAP_NAMES")
+	if snapNamesString != "" {
+		snapNames := strings.Split(snapNamesString, ", ")
+		log.Fatal(snapcraftExporter(listenPort, metricsPath, snapNames))
 	}
 }
 
-func homebrewExporter(listenPort string, metricsPath string, snapNames []string) error {
+func snapcraftExporter(listenPort string, metricsPath string, snapNames []string) error {
 	snapcraft := newSnapcraftCollector(snapNames)
 	prometheus.MustRegister(snapcraft)
 
