a67958bf829a5da0bca70b3e2ff0b0f96b3d973b
Ian Davis <jungziege@gmail.com>
2022-10-05 14:56:54 +0100

    Don't use date as a label

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -46,43 +47,43 @@ func newSnapcraftCollector(snapNames []string) *SnapcraftCollector {
 		SnapNames: snapNames,
 		deviceChangeDaily: prometheus.NewDesc("snapcraft_device_change_daily",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. daily_device_change: contains the 3 series representing the number of new, continued and lost devices with the given snap installed compared to the previous day.",
-			[]string{"snap", "date", "change"}, nil,
+			[]string{"snap", "change"}, nil,
 		),
 		deviceChangeWeekly: prometheus.NewDesc("snapcraft_device_change_weekly",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. weekly_device_change: similar to the ‘daily_device_change’ metric but operates on a 7 day window. i.e. new contains the number of devices that were seen during the last 7 days but not in the previous 7 day and so on for continued and lost.",
-			[]string{"snap", "date", "change"}, nil,
+			[]string{"snap", "change"}, nil,
 		),
 		installBaseByChannelDaily: prometheus.NewDesc("snapcraft_install_base_by_channel_daily",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. installed_base_by_channel: contains one series per channel representing the number of devices with the given snap installed, channels with no data across the entire interval are omitted.",
-			[]string{"snap", "date", "channel"}, nil,
+			[]string{"snap", "channel"}, nil,
 		),
 		installBaseByCountryDaily: prometheus.NewDesc("snapcraft_install_base_by_country_daily",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. installed_base_by_country: contains one series per country representing the number of devices with the given snap installed.",
-			[]string{"snap", "date", "country"}, nil,
+			[]string{"snap", "country"}, nil,
 		),
 		installBaseBySystemDaily: prometheus.NewDesc("snapcraft_install_base_by_system_daily",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. installed_base_by_operating_system: contains one series per operating_system representing the number of devices with the given snap installed.",
-			[]string{"snap", "date", "system"}, nil,
+			[]string{"snap", "system"}, nil,
 		),
 		installBaseByVersionDaily: prometheus.NewDesc("snapcraft_install_base_by_version_daily",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. installed_base_by_version: contains one series per version representing the number of devices with the given snap installed.",
-			[]string{"snap", "date", "version"}, nil,
+			[]string{"snap", "version"}, nil,
 		),
 		installBaseByChannelWeekly: prometheus.NewDesc("snapcraft_install_base_by_channel_weekly",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. weekly_installed_base_by_channel: similar to the installed_base_by_channel metric but operates in a 7 day window.",
-			[]string{"snap", "date", "channel"}, nil,
+			[]string{"snap", "channel"}, nil,
 		),
 		installBaseByCountryWeekly: prometheus.NewDesc("snapcraft_install_base_by_country_weekly",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. weekly_installed_base_by_country: similar to the installed_base_by_country metric but operates in a 7 day window.",
-			[]string{"snap", "date", "country"}, nil,
+			[]string{"snap", "country"}, nil,
 		),
 		installBaseBySystemWeekly: prometheus.NewDesc("snapcraft_install_base_by_system_weekly",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. weekly_installed_base_by_operating_system: similar to the installed_base_by_operating_system metric but operates in a 7 day window.",
-			[]string{"snap", "date", "system"}, nil,
+			[]string{"snap", "system"}, nil,
 		),
 		installBaseByVersionWeekly: prometheus.NewDesc("snapcraft_install_base_by_version_weekly",
 			"Exported from https://snapcraft.io/docs/snapcraft-metrics. weekly_installed_base_by_version: similar to the installed_base_by_version metric but operates in a 7 day window.",
-			[]string{"snap", "date", "version"}, nil,
+			[]string{"snap", "version"}, nil,
 		),
 	}
 }
@@ -118,8 +119,11 @@ func collectMetric(snapName string, snapMetricName string, metric *prometheus.De
 	snapcraftMetrics := getSnapcraftMetrics(snapName, snapMetricName)
 	for _, item := range snapcraftMetrics.Series {
 		for i, date := range snapcraftMetrics.Buckets {
-			m := prometheus.MustNewConstMetric(metric, prometheus.GaugeValue, float64(item.Values[i]), snapName, date, item.Name)
-			ch <- m
+			// metrics are on a 1 day lag, so date should match yesterday
+			if time.Now().AddDate(0, 0, -1).Format("2006-01-02") == date {
+				m := prometheus.MustNewConstMetric(metric, prometheus.GaugeValue, float64(item.Values[i]), snapName, item.Name)
+				ch <- m
+			}
 		}
 	}
 }
